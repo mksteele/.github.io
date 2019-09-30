4073e5c89bc5c45adc7bc36c6774c8a0511ca0b0
Miranda Steele <miranda@purestorage.com>
2019-09-29 21:53:53 -0700

    Adding different views

--- node-project/server.js
+++ node-project/server.js
@@ -5,16 +5,26 @@ const app = express();
 
 app.set('view engine', 'pug');
 
-// serve static files from the `static` folder
+// serve static files from the `assets` folder
 app.use(express.static(__dirname + '/assets'));
 
 const server = app.listen(7000, () => {
     console.log(`Express running → PORT ${server.address().port}`);
 });
+
+// By default, host index
 app.get('/', (req, res) => {
     res.render('index', {
-        sitename: "Miranda Steele",
-        title: "Home",
-        data: data.globals
+        sitename: data.sitename,
+        navigation: data.navigation,
+        title: ""
+    });
+});
+
+app.get("/:route", (req, res) => {
+    res.render(req.params.route, {
+        sitename: data.sitename,
+        title: req.params.route,
+        navigation: data.navigation
     });
 });
